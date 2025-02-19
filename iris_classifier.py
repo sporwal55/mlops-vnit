12e22f8c7d12b0dc751b717ace377d67a86fcb3f
sporwal55 <shivam5593@gmail.com>
2025-02-19 07:33:45 +0530

    Update iris_classifier.py

--- iris_classifier.py
+++ iris_classifier.py
@@ -12,7 +12,7 @@ y= iris.target
 ##Enable autologging
 mlflow.sklearn.autolog()
 #Step 2 Split data
-x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.4, random_state=42)
+x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.3, random_state=42)
 with mlflow.start_run():
     model = LogisticRegression(max_iter = 200)
     model.fit(x_train, y_train)
